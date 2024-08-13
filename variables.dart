e71febe0546aa42b25042dd851f3622023ab1acb
MutumaDerick <pdmutuma@gmail.com>
2024-08-13 17:04:52 +0300

    Introduction to dart

---
+++ variables.dart
@@ -0,0 +1,18 @@
+void main() {
+  //variables in dart
+  var lesson = "Variables in Dart";
+  const PI = 3.14;
+  final date = 12;
+
+  var name ="Derick";
+  var age = 23;
+  var height = 5.6;
+
+  print(lesson);
+  print(PI);
+  print(date);
+
+  print(name);
+  print(age);
+  print(height);
+}
